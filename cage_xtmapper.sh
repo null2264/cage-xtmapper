571de53c4bb47fe814edf95432a966f6293a9e1a
Ahmad Ansori Palembani <palembani@gmail.com>
2024-07-26 11:24:48 +0700

    fix: Fixed more issue when trying to launch WayDroid

--- cage_xtmapper.sh
+++ cage_xtmapper.sh
@@ -62,10 +62,10 @@ case $(waydroid status | head -n1) in
 esac
 
 su "$user" --command "./build/cage waydroid show-full-ui" | (
-	while ! waydroid prop get sys.boot_completed 2>/dev/null; do
+	while [ -z $(waydroid shell getprop sys.boot_completed) ]; do
 		sleep 1;
-	done
+	done;
 
-	waydroid shell -- sh -c 'echo "/system/bin/app_process -Djava.library.path=$(echo /data/app/*/xtr.keymapper*/lib/x86_64) -Djava.class.path=$(echo /data/app/*/xtr.keymapper*/base.apk) / xtr.keymapper.server.RemoteServiceShell --wayland-client" > /tmp/xtmapper.sh' && \
+	waydroid shell -- sh -c 'echo "/system/bin/app_process -Djava.library.path=$(echo /data/app/*/xtr.keymapper*/lib/x86_64) -Djava.class.path=$(echo /data/app/*/xtr.keymapper*/base.apk) / xtr.keymapper.server.RemoteServiceShell \"\$@\"" > /tmp/xtmapper.sh' && \
 	exec waydroid shell -- sh /tmp/xtmapper.sh --wayland-client --width=$XTMAPPER_WIDTH --height=$XTMAPPER_HEIGHT
 )
